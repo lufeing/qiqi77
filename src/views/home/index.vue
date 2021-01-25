49971b6f3b04145573e4436ba55652944c5159a0
xing_ye <787747709@qq.com>
2021-01-25 10:53:25 +0800

    1.搭建vue3.0 + typeScript环境 next:引入并包装axios

---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<!--  -->
+<template>
+	<div @click="addCount">{{count}}</div>
+</template>
+
+<script lang='ts'>
+	import { ref, Ref } from "vue";
+	export default {
+		setup() {
+			const count: Ref = ref(0);
+			const addCount = (): void => {
+				count.value += 1;
+			};
+			return { count, addCount };
+		},
+	};
+</script>
+
+<style lang='scss' scoped>
+</style>
